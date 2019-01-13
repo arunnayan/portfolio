c67dbf1b9ab00bf0737fce4ef93f0b710fee130a
arun singh <arunnayan.28@gmail.com>
2019-01-13 14:52:42 -0800

    Added porfolio section

--- js/script.js
+++ js/script.js
@@ -50,13 +50,44 @@ $(document).ready(function() {
 			}
 		});
 	 }
- })
- if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200){
- $('.counter').each(function(){
-	 var element = $(this);
-	 var endVal = parseInt(element.text());
-	 element.countup(endVal);
-	 countUpFinished = true;
- })
-}
+	 if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200){
+		$(".counter").each(function() {
+			var element = $(this);
+			var endVal = parseInt(element.text());
+
+			element.countup(endVal);
+		})
+
+		countUpFinished = true;
+
+	}
+
+ });
+
+$("[data-fancybox]").fancybox();
+
+$(".items").isotope({
+	filter: '*',
+	animationOptions: {
+		duration: 1500,
+		easing: 'linear',
+		queue: false
+	}
+});
+
+$("#filters a").click(function() {
+	$("#filters .current").removeClass("current");
+	$(this).addClass("current");
+	var selector = $(this).attr("data-filter");
+	$(".items").isotope({
+		filter: selector,
+		animationOptions: {
+			duration: 1500,
+			easing: 'linear',
+			queue: false
+		}
+	});
+	return false;
+});
+
 });
