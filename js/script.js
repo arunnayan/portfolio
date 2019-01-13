6902ada0485a6f38aae79dbf4e93d0b090df8609
arun singh <arunnayan.28@gmail.com>
2019-01-12 22:51:29 -0800

    added stats section

--- js/script.js
+++ js/script.js
@@ -33,6 +33,9 @@ $(document).ready(function() {
 
 	
  var skillsTopOffset = $(".skillSection").offset().top;
+ var statsTopOffset = $(".statsSection").offset().top;
+ var countUpFinished = false;
+
  $(window).scroll(function(){
 	 if(window.pageYOffset > skillsTopOffset - $(window).height() + 200){
 		$('.chart').easyPieChart({
@@ -48,4 +51,12 @@ $(document).ready(function() {
 		});
 	 }
  })
+ if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200){
+ $('.counter').each(function(){
+	 var element = $(this);
+	 var endVal = parseInt(element.text());
+	 element.countup(endVal);
+	 countUpFinished = true;
+ })
+}
 });
